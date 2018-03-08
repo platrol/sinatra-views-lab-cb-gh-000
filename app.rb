bcf44267f7c4337a7b0d9ed408f3ac9eda69cb93
norman lewis <takialog@gmail.com>
2018-03-08 13:55:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,14 +5,14 @@ class App < Sinatra::Base
 	get '/hello' do
 		erb :hello
 	end
-
-	get '/date' do
-		erb :goodbye
-	end
-
 	get '/date' do
 		erb :date
 	end
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+
 
 
 end
